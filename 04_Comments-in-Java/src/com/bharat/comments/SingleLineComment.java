dbdb81d04f2806a8bd7b7082f994025843f7d34d
Bharat2044 <bk9955204456@gmail.com>
2024-03-11 22:20:37 +0530

    code added

---
+++ 04_Comments-in-Java/src/com/bharat/comments/SingleLineComment.java
@@ -0,0 +1,12 @@
+package com.bharat.comments;
+
+public class SingleLineComment {
+    
+    public static void main(String args[]) {
+        
+        System.out.println("Single Line Comment");
+        
+        // This is sinlge line comment
+        System.out.println("Hello, World!!!");
+    }
+}
