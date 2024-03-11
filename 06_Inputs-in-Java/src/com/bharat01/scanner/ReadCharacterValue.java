a7c3e1fc71edfdf7d333b40f172569de5c977df0
Bharat2044 <bk9955204456@gmail.com>
2024-03-11 22:22:23 +0530

    code added

---
+++ 06_Inputs-in-Java/src/com/bharat01/scanner/ReadCharacterValue.java
@@ -0,0 +1,14 @@
+package com.bharat01.scanner;
+
+import java.util.Scanner;
+
+public class ReadCharacterValue {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a character: ");
+        char a = sc.next().charAt(0);
+        
+        System.out.println("The Character: " + a);
+    }
+}
