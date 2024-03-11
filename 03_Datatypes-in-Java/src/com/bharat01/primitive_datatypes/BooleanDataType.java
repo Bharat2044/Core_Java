a515fa785a3a01dd5914240ea2fa061b7fedde30
Bharat2044 <bk9955204456@gmail.com>
2024-03-11 22:19:23 +0530

    code added

---
+++ 03_Datatypes-in-Java/src/com/bharat01/primitive_datatypes/BooleanDataType.java
@@ -0,0 +1,17 @@
+package com.bharat01.primitive_datatypes;
+
+public class BooleanDataType {
+	static boolean x;
+    
+    public static void main(String args[]) {
+    	System.out.println("Default value of boolean = " + x);
+    	
+        boolean a = true;     
+        boolean b = false;    
+        
+        // boolean c = 0;  // BooleanDataType.java:12: error: incompatible types: int cannot be converted to boolean
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
