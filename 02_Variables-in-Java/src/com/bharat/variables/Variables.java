929ea2f787faf8aa5ad6a8ecd145eeea2831de4a
Bharat2044 <bk9955204456@gmail.com>
2024-03-11 22:16:05 +0530

    code added

---
+++ 02_Variables-in-Java/src/com/bharat/variables/Variables.java
@@ -0,0 +1,20 @@
+package com.bharat.variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// These all are local variables
+		
+		String name = "Bharat";
+		int age = 21;
+		boolean isStudent = true;
+		double marks = 99.99;
+		char section = 'A';
+		
+		System.out.println("Name = " + name);
+		System.out.println("Age = " + age);
+		System.out.println("Is Student = " + isStudent);
+		System.out.println("Marks = " + marks);
+		System.out.println("Section = " + section);
+	}
+}
