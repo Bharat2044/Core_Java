e39bb2b842fd1bddf09292bb7b63bcaac26cc424
Bharat2044 <bk9955204456@gmail.com>
2024-03-12 13:12:38 +0530

    code added

---
+++ 11_Selection-or-Conditional-Statement-in-Java/src/com/bharat01/if_statement02/if_else/CheckNumberIsPositiveOrNegative.java
@@ -0,0 +1,19 @@
+package com.bharat01.if_statement02.if_else;
+
+import java.util.Scanner;
+
+public class CheckNumberIsPositiveOrNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+    
+        if (num >= 0) {
+            System.out.println("You entered a positive integer: " + num);
+        }
+        else {
+            System.out.println("You entered a negative integer: " + num);
+        }
+    }
+}
---
+++ 11_Selection-or-Conditional-Statement-in-Java/src/com/bharat03/switch02/switch_expression/SwitchExpressionEx.java
@@ -0,0 +1,19 @@
+package com.bharat03.switch02.switch_expression;
+
+public class SwitchExpressionEx {
+    public static void main(String[] args) {
+        int num = 3;
+    
+        switch(num) {
+            case 1 -> System.out.println("Number is " + num);
+                
+            case 2 -> System.out.println("Number is " + num);
+                
+            case 3 -> System.out.println("Number is " + num);
+                
+            case 4 -> System.out.println("Number is " + num);
+                
+            default -> System.out.println("Default Case");
+        }
+    }
+}
