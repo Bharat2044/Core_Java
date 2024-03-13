eeb10b40b9c59e6bdf45a3b3d48b4ff233144a42
Bharat2044 <bk9955204456@gmail.com>
2024-03-13 11:01:34 +0530

    code added

---
+++ 15_Loop-Programs/src/com/bharat/loop_programs/_21_FactorialOfANumber.java
@@ -0,0 +1,25 @@
+package com.bharat.loop_programs;
+
+import java.util.Scanner;
+
+public class _21_FactorialOfANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int num = sc.nextInt();
+        
+        if(num < 0) {
+            System.out.println("Factorial of negative number is not defined 😜");
+        }
+        else {
+            int fact = 1;
+            
+            for(int i=2; i<=num; i++) {
+                fact *= i;
+            } 
+        
+            System.out.println(num + "! = " + fact);
+        }
+    }
+}
