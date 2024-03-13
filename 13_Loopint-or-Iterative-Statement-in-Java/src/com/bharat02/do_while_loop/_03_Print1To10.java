1c5974d7b6f14f1cb31626961a590747d02cec2e
Bharat2044 <bk9955204456@gmail.com>
2024-03-13 10:53:24 +0530

    code added

---
+++ 13_Loopint-or-Iterative-Statement-in-Java/src/com/bharat02/do_while_loop/_03_Print1To10.java
@@ -0,0 +1,12 @@
+package com.bharat02.do_while_loop;
+
+public class _03_Print1To10 {
+    public static void main(String[] args) {
+        int i = 1; 
+    
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 10);
+    }
+}
