bd72dbc5ef4cbdcf77513f40276464f2142e988d
Bharat2044 <bk9955204456@gmail.com>
2024-03-13 10:54:56 +0530

    code added

---
+++ 13_Loopint-or-Iterative-Statement-in-Java/src/com/bharat03/for_loop/_02_ForLoopEx2.java
@@ -0,0 +1,10 @@
+package com.bharat03.for_loop;
+
+public class _02_ForLoopEx2 {
+    public static void main(String[] args) {
+    
+        for(int i=5; i>=1; i--) {
+            System.out.println("Hello");
+        }
+    }
+}
