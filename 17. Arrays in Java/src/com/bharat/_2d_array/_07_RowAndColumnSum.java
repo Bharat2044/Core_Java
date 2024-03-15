fe2e43099a0424ab0a79aae98b1fae05fd5fca6e
Bharat2044 <bk9955204456@gmail.com>
2024-03-15 13:37:46 +0530

    code added

---
+++ 17. Arrays in Java/src/com/bharat/_2d_array/_07_RowAndColumnSum.java
@@ -0,0 +1,39 @@
+package com.bharat._2d_array;
+
+public class _07_RowAndColumnSum {
+
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int m = 3, n = 3;
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(arr[i][j] + "  ");
+            System.out.println();
+        }
+        System.out.println();
+
+        
+        // Calculate row sums
+        System.out.print("Array row sums are: ");
+        for (int i = 0; i < m; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++)
+                sum += arr[i][j];
+            System.out.print(sum + "  ");
+        }
+        System.out.println();
+
+        
+        // Calculate column sums
+        System.out.print("Array column sums are: ");
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++)
+                sum += arr[j][i];
+            System.out.print(sum + "  ");
+        }
+        System.out.println();
+    }
+}
