15018ca6cd7089b58e8a0db35b1fc0e17cd97307
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:06:12 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_01_MethodsEx1.java
@@ -0,0 +1,12 @@
+package com.bharat.methods;
+
+public class _01_MethodsEx1 { 
+
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
+    
+    public static void main(String[] args) {
+        sayHi();
+    }
+}
