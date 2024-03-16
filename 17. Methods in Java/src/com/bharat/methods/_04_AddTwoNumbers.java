5c559313a2523a93342e9f198b5f834964623f5d
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:06:31 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_04_AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.bharat.methods;
+
+public class _04_AddTwoNumbers { 
+
+    public static void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        
+        sum(a, b);
+    }
+}
