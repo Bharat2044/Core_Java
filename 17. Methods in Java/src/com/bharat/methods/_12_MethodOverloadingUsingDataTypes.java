b79f07afacc2557fac03a92bcb570384a2c95a16
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:07:16 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_12_MethodOverloadingUsingDataTypes.java
@@ -0,0 +1,28 @@
+package com.bharat.methods;
+
+public class _12_MethodOverloadingUsingDataTypes {
+  
+    public static int sum(int a, int b) {
+        return (a + b);
+    }
+  
+    public static float sum(float a, int b) {
+        return (a + b);
+    }
+  
+    public static float sum(float a, float b) {
+        return (a + b);
+    }
+  
+    public static double sum(double a, double b) {
+        return (a + b);
+    }
+  
+    public static void main(String[] args) {
+        
+        System.out.println(sum(2, 3));
+        System.out.println(sum(3.3f, 3));
+        System.out.println(sum(2.1f, 3.1f));
+        System.out.println(sum(4.2, 5.5));
+    }
+}
