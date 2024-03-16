875f640452126d9bdfe3b80e127c468411aa2e45
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:08:59 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_29_FactorialOfANumber.java
@@ -0,0 +1,35 @@
+package com.bharat.methods;
+import java.util.Scanner;
+
+public class _29_FactorialOfANumber {
+  
+    public static int factorial(int n) {
+        if(n <= 1) {
+           return 1;
+        }
+        
+        int fact = 1;
+        for(int i=2; i<=n; i++) {
+            fact *= i;
+        } 
+        
+        return fact;
+    }
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a positive number: ");
+        int num = sc.nextInt();
+        
+        if(num < 0) {
+            System.out.println("Factorial of negative number is not defined 😜");
+            // System.exit(0);
+            return;
+        }
+        
+        int ans = factorial(num);
+        
+        System.out.println(num + "! = " + ans);
+    }
+}
