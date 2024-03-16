93dfa53dc83b0fa8c01f2ff932ef01602bd9d91b
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:07:49 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_17_BlockScopeEx2.java
@@ -0,0 +1,24 @@
+package com.bharat.methods;
+
+public class _17_BlockScopeEx2 {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println("Value of a before the block = " + a);
+        
+        {
+            System.out.println("Value of a inside block = " + a);
+            // int a = 5;  // BlockScopeEx2.java:10: error: variable a is already defined in method main(String[])
+        
+            a = 100;
+            System.out.println("Value of a inside block after modifing = " + a);
+            
+            int b = 20;
+            System.out.println("Value of b inside block = " + b);
+        }
+        
+        int b = 99;
+        System.out.println("Value of b outside the block = " + b);  
+        
+        System.out.println("Value of a after the block = " + a);
+    }
+}
