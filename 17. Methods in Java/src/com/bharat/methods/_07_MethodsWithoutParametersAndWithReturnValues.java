121ae1251252d7199998a96bfe5d02fec030a0aa
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:06:56 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_07_MethodsWithoutParametersAndWithReturnValues.java
@@ -0,0 +1,23 @@
+package com.bharat.methods;
+
+import java.util.Scanner;
+
+public class _07_MethodsWithoutParametersAndWithReturnValues { 
+
+    public static int sum() {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();        
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        
+        return (a + b);
+    }
+    
+    public static void main(String[] args) {        
+        int ans = sum();
+        
+        System.out.println("Sum = " + ans);
+    }
+}
