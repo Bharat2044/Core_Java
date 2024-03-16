bfab6b766d3edb32fc4f6be813bcb0d1ff9ce295
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:12:13 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_54_AreaAndnCircumferenceOfCircle.java
@@ -0,0 +1,31 @@
+// Write a program to print the circumference and area of a circle of radius entered by user by defining your own method.
+
+
+package com.bharat.methods;
+
+import java.util.Scanner;
+
+public class _54_AreaAndnCircumferenceOfCircle {
+  
+    public static double area(int r) {
+        
+        return (Math.PI * r * r);
+    }
+  
+    public static double circumference(int r) {
+        
+        return (2* Math.PI * r);
+    }
+  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of the Circle: ");
+        int r = sc.nextInt();
+        
+        System.out.println("Area of Circle = " + area(r));
+        System.out.println("nCircumference of Circle = " + circumference(r));
+        
+        sc.close();
+    }
+}
