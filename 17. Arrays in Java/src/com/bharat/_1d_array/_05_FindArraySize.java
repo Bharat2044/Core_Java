5d175aaf9dce1b1404a8642ca99686e61eae31e9
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:05:55 +0530

    code added

--- 17. Arrays in Java/src/com/bharat/_1d_array/_05_FindArraySize.java
+++
@@ -1,9 +0,0 @@
-package com.bharat._1d_array;
-
-public class _05_FindArraySize {
-	public static void main(String[] args) {
-		int[] arr = {10, 20, 30 , 40, 50};
-		
-		System.out.println("Size of the array = " + arr.length);
-	}
-}
