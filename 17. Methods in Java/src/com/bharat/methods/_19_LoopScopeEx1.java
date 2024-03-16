a80e2876f3dd8f303b7ed5958889b36737730b19
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:07:55 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_19_LoopScopeEx1.java
@@ -0,0 +1,21 @@
+package com.bharat.methods;
+
+public class _19_LoopScopeEx1 {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println("Value of a before the for loop = " + a);
+        
+        for(int i=1; i<=5; i++) {
+            // int a = 10; // LoopScopeEx1.java:9: error: variable a is already defined in method main(String[])
+            int num = i;
+            a = 99;
+            
+            System.out.println(a + "  " + num);
+        }
+        
+        
+        // System.out.println("Value of num after the for loop " + num);   // LoopScopeEx1.java:17: error: cannot find symbol l  =>   symbol: variable num     location: class LoopScopeEx1
+        
+        System.out.println("Value of a after the for loop = " + a);
+    }
+}
