1813543a691ce2ae4d15d9e7d45677adbd33af52
Bharat2044 <bk9955204456@gmail.com>
2024-03-16 12:06:45 +0530

    code added

---
+++ 17. Methods in Java/src/com/bharat/methods/_05_MultiplyTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.bharat.methods;
+
+public class _05_MultiplyTwoNumbers { 
+
+    public static void product(int a, int b) {
+        System.out.println(a * b);
+    }
+    
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        
+        product(a, b);
+    }
+}
