da1896286bf326a37f0c42ea1bf41558f23d5aaf
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:51:43 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_05_ReadFloatValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _05_ReadFloatValue {
+    public static void main(String args[]) {
+        
+        float a = Float.parseFloat(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
