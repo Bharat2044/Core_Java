d578db69190bc047752163f431db68b458081592
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:52:12 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_01_ReadByteValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _01_ReadByteValue {
+	public static void main(String args[]) {
+		
+		byte a = Byte.parseByte(args[0]);
+
+		System.out.println("The number is: " + a);
+	}
+}
