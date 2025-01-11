e0065536255b4e2f89dd22bda8de45b8de3b7e76
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:51:48 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_06_ReadDoubleValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _06_ReadDoubleValue {
+    public static void main(String args[]) {
+        
+        double a = Double.parseDouble(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
