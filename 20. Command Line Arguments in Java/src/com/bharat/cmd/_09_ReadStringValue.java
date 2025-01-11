47e9bdcb6288ba6e9095bd2015b6e2ebac8ba89b
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:52:30 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_09_ReadStringValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _09_ReadStringValue {
+    public static void main(String args[]) {
+        
+        String a = args[0];
+        
+        System.out.println("String: " + a);
+    }
+}
