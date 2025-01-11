ef05b510e106605e192a8d1c59987daecfef399f
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:51:37 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_03_ReadIntegerValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _03_ReadIntegerValue {
+    public static void main(String args[]) {
+        
+        int a = Integer.parseInt(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
