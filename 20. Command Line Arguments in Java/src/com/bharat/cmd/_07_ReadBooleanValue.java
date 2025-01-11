c8cc61ae383d69e5e3cf01b43041e99dc2c35c37
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:52:06 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_07_ReadBooleanValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _07_ReadBooleanValue {
+    public static void main(String args[]) {
+        
+        boolean a = Boolean.parseBoolean(args[0]);
+        
+        System.out.println("Result: " + a);
+    }
+}
