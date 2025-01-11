d70da81a93665c528ac01feef63ac3dc66070684
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:52:00 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_04_ReadLongValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _04_ReadLongValue {
+    public static void main(String args[]) {
+        
+        long a = Long.parseLong(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
