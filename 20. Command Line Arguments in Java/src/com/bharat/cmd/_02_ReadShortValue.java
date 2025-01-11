e35bb08935426c82b830a21a70dda3705934e3b8
Bharat Kumar <bk9955204456@gmail.com>
2025-01-11 13:51:32 +0530

    code added

---
+++ 20. Command Line Arguments in Java/src/com/bharat/cmd/_02_ReadShortValue.java
@@ -0,0 +1,10 @@
+package com.bharat.cmd;
+
+public class _02_ReadShortValue {
+    public static void main(String args[]) {
+        
+        short a = Short.parseShort(args[0]);
+        
+        System.out.println("The number is: " + a);
+    }
+}
