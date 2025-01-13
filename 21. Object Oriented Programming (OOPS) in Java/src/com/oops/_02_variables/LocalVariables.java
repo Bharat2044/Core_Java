9fe1cc37b6d9e953297e1307f1296e496d20213c
Bharat Kumar <bk9955204456@gmail.com>
2025-01-13 16:56:40 +0530

    code added

---
+++ 21. Object Oriented Programming (OOPS) in Java/src/com/oops/_02_variables/LocalVariables.java
@@ -0,0 +1,16 @@
+package com.oops._02_variables;
+
+public class LocalVariables {
+	
+	public static void main(String[] args) {
+		
+		// Local Variables
+		int a = 20;
+		int b = 20;
+		String name = "Bharat";
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(name);
+	}
+}
