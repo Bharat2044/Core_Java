af7e4f5178f398a643b5f64c6f3ebbb3babce044
Bharat Kumar <bk9955204456@gmail.com>
2025-01-13 16:56:52 +0530

    code added

---
+++ 21. Object Oriented Programming (OOPS) in Java/src/com/oops/_03_methods/_01_Greet.java
@@ -0,0 +1,12 @@
+package com.oops._03_methods;
+
+public class _01_Greet {
+
+	public static void greet() {
+		System.out.println("Hi, Everyone!");
+	}
+
+	public static void main(String[] args) {
+		greet();
+	}
+}
